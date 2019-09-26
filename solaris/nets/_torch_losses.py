f7544f7b51dcf0d78edd5bf3d90077728897524f
nrweir <nrweir@users.noreply.github.com>
2019-09-26 15:28:13 -0400

    clamping output values before bce in focal

--- solaris/nets/_torch_losses.py
+++ solaris/nets/_torch_losses.py
@@ -62,6 +62,7 @@ class TorchFocalLoss(nn.Module):
         if self.logits:
             BCE_loss = F.binary_cross_entropy_with_logits(outputs, targets)
         else:
+            outputs = outputs.clamp(0,1)  # preclude runtimeerror
             BCE_loss = F.binary_cross_entropy(outputs, targets)
         pt = torch.exp(-BCE_loss)
         F_loss = (1-pt)**self.gamma * BCE_loss
